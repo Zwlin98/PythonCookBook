269a969ad18fa1cd9b41377d79c3703cf7938ad6
GaintZwlin <giantzwlin@gmail.com>
2020-01-31 21:13:28 +0800

    re.split()

--- Ch02/2-1.py
+++ Ch02/2-1.py
@@ -1,4 +1,13 @@
 # Create by Zwlin
 import re
-p = re.compile('ab*')
-print(p)
+line = 'asdf fjdk; afed, fjek,asdf,    foo'
+# 多分隔符分割
+res1 = re.split(r'[;,\s]\s*',line)
+# 使用捕获组时分割时会同时提取分隔符
+res2 =re.split(r'(;|,|\s)\s*',line)
+# 若想使用括号，但不要分隔符，使用非捕获组
+res3 = re.split(r'(?:;|,|\s)\s*',line)
+
+print(res1)
+print(res2)
+print(res3)
