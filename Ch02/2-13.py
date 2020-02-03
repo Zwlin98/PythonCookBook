5f0337c1b94b73bf5d996ca1d026bfcf84cd6a88
GaintZwlin <giantzwlin@gmail.com>
2020-02-03 18:13:43 +0800

    format function

---
+++ Ch02/2-13.py
@@ -0,0 +1,18 @@
+# Create by Zwlin
+text = 'Hello World'
+
+print(text)
+
+print(text.center(20))
+
+print(text.center(20,'*'))
+
+print(format(text,'^20'))
+
+print(format(text,'=^20'))
+
+x = 1.2345
+
+print(format(x,'>10'))
+
+print(format(x,'>10.2f'))
