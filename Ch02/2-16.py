c7cc0d145f26a50b4c0d93d1a8b840c8df327c5a
GaintZwlin <giantzwlin@gmail.com>
2020-02-05 20:04:03 +0800

    string

---
+++ Ch02/2-16.py
@@ -0,0 +1,11 @@
+# Create by Zwlin
+
+s = "Look into my eyes, look into my eyes, the eyes, the eyes, \
+the eyes, not around the eyes, don't look around the eyes, \
+look into my eyes, you're under."
+
+import textwrap
+
+print(textwrap.fill(s,70))
+
+print(textwrap.fill(s,40))
---
+++ Ch02/2-17.py
@@ -0,0 +1,9 @@
+# Create by Zwlin
+
+s = 'Elements are written as "<tag>text</tag>".'
+
+import html
+
+print(s)
+
+print(html.escape(s))
