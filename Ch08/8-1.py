54b9f8fc4a9746ec05092fc36708475a5be3fb3b
GaintZwlin <giantzwlin@gmail.com>
2020-02-06 22:36:13 +0800

    class

---
+++ Ch08/8-1.py
@@ -0,0 +1,11 @@
+# Create by Zwlin
+class Pair:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Pair({0.x!r},{0.y!r})'.format(self)
+
+    def __str__(self):
+        return '({0.x!s},{0.y!s})'.format(self)
