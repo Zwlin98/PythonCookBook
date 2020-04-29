0ce16d107c91161c6c53ab73e8b750e0918aebf3
GiantZwlin <giantzwlin@gmail.com>
2020-04-29 11:52:18 +0800

    up to 1-5

---
+++ Ch01/class1_5.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self.index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self.index, item))
+        self.index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
